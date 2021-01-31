cc865f348438c3a0c56c9147e2098e9c6a615f40
Daulen Zhangabylov <daulenz@gmail.com>
2021-01-31 22:11:49 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,12 +1,11 @@
 
 def loop_message_five_times(message)
   counter=0
-while message[counter] do
+while counter<5  do
 puts message[counter]
 counter+=1
 	  end
 	end
-loop_message_five_times=["Hello World.\n","Hello World.\n","Hello World.\n","Hello World.\n","Hello World.\n"]
 
 
 
