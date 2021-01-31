bc65dc2f14f8b1aaabf50a16979a33faab877797
Daulen Zhangabylov <daulenz@gmail.com>
2021-01-31 21:13:14 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,17 @@
-# Write your methods here
+
+def loop_message_five_times
+  
+
+
+
+
+
+
+# describe 'loop_message_five_times' do
+  
+  it 'takes in a message and outputs it five times' do
+    message = "Hello World."
+    expect { loop_message_five_times(message) }.to output(/Hello World.\n/).to_stdout
+    expect { loop_message_five_times(message) }.to output(/Hello World.\nHello World.\nHello World.\nHello World.\nHello World./).to_stdout, "Expected the input message to be output five times using puts:\n\nHello World.\nHello World.\nHello World.\nHello World.\nHello World.\n"
+  end
+endWrite your methods here
